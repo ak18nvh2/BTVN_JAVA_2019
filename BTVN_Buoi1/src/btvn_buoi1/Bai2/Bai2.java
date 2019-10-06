9dfc3f5b0289f74796659120e891424b14b9af54
HXThai <thaichelsea99@gmail.com>
2019-10-06 19:56:25 +0700

    Nop bai tap buoi 1

---
+++ BTVN_Buoi1/src/btvn_buoi1/Bai2/Bai2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btvn_buoi1.Bai2;
+
+/**
+ *
+ * @author ThaiMai
+ */
+public class Bai2 {
+    
+}
